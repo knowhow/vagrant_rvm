f164a4acaf7a4a1046ac62a1863b6a6e8a55ec92
Ernad Husremovic <hernad@bring.out.ba>
2011-06-03 00:08:48 +0200

    json ne trebamo

--- cookbooks/rvm/files/default/rvm.sh
+++ cookbooks/rvm/files/default/rvm.sh
@@ -1,3 +1,2 @@
-# this doesn't work for me!
-# [[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"  # This loads RVM into a shell session.
-scripts /usr/local/rvm/scripts/rvm
+[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"  # This loads RVM into a shell session.
+# source /usr/local/rvm/scripts/rvm
